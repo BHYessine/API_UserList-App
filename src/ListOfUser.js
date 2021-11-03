4ab705c572a4ac4d0e8e14553a94cf0cca26b256
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-11-03 14:16:54 +0100

    Update ListOfUser.js

--- src/ListOfUser.js
+++ src/ListOfUser.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { 
+  BsFillPersonFill, 
+  BsFillHouseDoorFill, 
+  BsFillEnvelopeFill, 
+  BsFillTelephoneFill,
+  BsGlobe,
+  BsFillBriefcaseFill 
+} from 'react-icons/bs';
+import user from '../public/user.png'
 
 function ListOfUser(props) {
     
     return (
-        <div>
-           <li>
-                 name:    {props.user.name} <br/>
-                 email: {props.user.email} <br/>
-                 city: {props.user.address.city} <br/>
-            </li>
+        <div className="userCard">
+          <div className="userImg">
+              <img 
+                src= {user}
+                title={props.user.username} 
+                alt="user"                 
+                />
+              <h4> {props.user.username} </h4>
+          </div>
+           <ul key={props.user.id} className="userBio">
+                <li> <BsFillPersonFill className="icon"/> {props.user.name} </li>
+                <li> <BsFillHouseDoorFill className="icon"/> {props.user.address.street} {props.user.address.suite}, {props.user.address.city} </li>
+                <li> <BsFillEnvelopeFill className="icon"/> {props.user.email} </li>
+                <li> <BsFillTelephoneFill className="icon"/> {props.user.phone} </li>
+                <li> <BsGlobe className="icon"/> {props.user.website} </li>
+                <li> <BsFillBriefcaseFill className="icon"/> {props.user.company.bs} at {props.user.company.name} </li>
+            </ul>
         </div>
     )
 }
